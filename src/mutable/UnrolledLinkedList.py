c70356925cba6bed6b1b4167fbbf21f39452238c
xhjIzmy <705071226@qq.com>
2021-03-22 19:29:39 +0800

    mutable version: implement of function to_list()、from_list()、remove()、reverse()

--- src/mutable/UnrolledLinkedList.py
+++ src/mutable/UnrolledLinkedList.py
@@ -19,10 +19,60 @@ class UnrolledLinkedList:
         return self.total_size
 
     def add(self, element):
-        self.insert(self.tail, self.tail.node_size, element)
+        self._insert(self.tail, self.tail.node_size, element)
         return self
 
-    def insert(self, node, position, element):
+    def to_list(self):
+        res = []
+        node = self.head
+        while node is not None:
+            for ele in node.elements[0:node.node_size]:
+                res.append(ele)
+            node = node.next
+        return res
+
+    def from_list(self, lst):
+        if len(lst) > 0:
+            for e in lst:
+                self.add(e)
+        return self
+
+    def remove(self, value):
+        node = self.head
+        while node is not None:
+            index = 0
+            for ele in node.elements[0:node.node_size]:
+                if ele == value:
+                    self._remove_from_node(node, index)
+                index += 1
+            node = node.next
+        return self
+
+    def reverse(self):
+        lst = self.to_list()
+        for ele in lst:
+            self.remove(ele)
+        lst.reverse()
+        return self.from_list(lst)
+
+    def _remove_from_node(self, node, index):
+        if node.node_size == 1:
+            if node.previous is not None:
+                node.previous.next = node.next
+            if node.next is not None:
+                node.next.previous = node.previous
+            node.next = None
+            node.previous = None
+            node.elements[index] = None
+            node.node_size -= 1
+            self.total_size -= 1
+        else:
+            for i in range(index, node.node_size-1):
+                node.elements[i] = node.elements[i+1]
+            node.node_size -= 1
+            self.total_size -= 1
+
+    def _insert(self, node, position, element):
         if node.node_size == self.nodeCapacity:
             new_node = Node(self.nodeCapacity)
             element_to_move = node.node_size//2
--- src/mutable/UnrolledLinkedListTest.py
+++ src/mutable/UnrolledLinkedListTest.py
@@ -13,6 +13,31 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(UnrolledLinkedList(5).add(1).add(2).add(3).add(4)
                          .add(5).add(6).add(7).size(), 7)
 
+    def test_to_list(self):
+        self.assertEqual(UnrolledLinkedList(5).to_list(), [])
+        self.assertEqual(UnrolledLinkedList(5).add(1).add(2).add(3).to_list(), [1, 2, 3])
+
+    def test_from_list(self):
+        test_data = [
+            [],
+            [1, 2],
+            [1, 2, 3, 4, 5, 6, 7]
+        ]
+        for e in test_data:
+            lst = UnrolledLinkedList(5).from_list(e)
+            self.assertEqual(lst.to_list(), e)
+
+    def test_remove(self):
+        self.assertEqual(UnrolledLinkedList(5).remove(1).to_list(), [])
+        self.assertEqual(UnrolledLinkedList(5).add(1).remove(1).to_list(), [])
+        self.assertEqual(UnrolledLinkedList(5).add(1).add(2).add(3).add(4)
+                         .add(5).add(6).add(7).remove(6).to_list(), [1, 2, 3, 4, 5, 7])
+
+    def test_reverse(self):
+        self.assertEqual(UnrolledLinkedList(5).reverse().to_list(), [])
+        self.assertEqual(UnrolledLinkedList(5).add(1).add(2).add(3)
+                         .reverse().to_list(), [3, 2, 1])
+
 
 if __name__ == '__main__':
     unittest.main()
